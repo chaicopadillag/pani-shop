88a3ba1c0e54cc1f91c050b31c59cb4349efa7f7
chaicopadillag <chaicopadillag@gmail.com>
2020-10-27 15:49:44 -0500

    Push Pani Shop GitHub

---
+++ frontend/controladores/carrito.controlador.php
@@ -0,0 +1,48 @@
+<?php
+
+class ControladorCarrito{
+
+	/*=============================================
+	MOSTRAR TARIFAS
+	=============================================*/
+
+	public function ctrMostrarTarifas(){
+
+		$tabla = "comercio";
+
+		$respuesta = ModeloCarrito::mdlMostrarTarifas($tabla);
+
+		return $respuesta;
+
+	}	
+
+	/*=============================================
+	NUEVAS COMPRAS
+	=============================================*/
+
+	static public function ctrNuevasCompras($datos){
+
+		$tabla = "compras";
+
+		$respuesta = ModeloCarrito::mdlNuevasCompras($tabla, $datos);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	VERIFICAR PRODUCTO COMPRADO
+	=============================================*/
+
+	static public function ctrVerificarProducto($datos){
+
+		$tabla = "compras";
+
+		$respuesta = ModeloCarrito::mdlVerificarProducto($tabla, $datos);
+	 
+	    return $respuesta;
+
+		
+	}
+
+}
